1d737b2d776221bd865854b6e3e3c5f25109d5d2
adilgame12 <adilgame12@gmail.com>
2024-06-30 21:38:38 +0700

    change to plural word

--- routers/bookRouter.go
+++ routers/bookRouter.go
@@ -11,7 +11,7 @@ func StartServer() *gin.Engine {
 
 	router.POST("/book", controllers.CreateBook)
 	router.GET("/book/:id", controllers.GetBook)
-	router.GET("/books", controllers.GetBooks)
+	router.GET("/books", controllers.GetBook)
 	router.DELETE("/book/:id", controllers.DeleteBook)
 	router.PUT("/book/:id", controllers.UpdateBook)
 	
